12124b44b3e626d8054df43b2ad81b18a97ff17b
Priyanshu <priyanshu21416@gmail.com>
2025-08-23 16:21:24 +0530

    Deploying frontend

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import "../App.css"
 import { useNavigate } from 'react-router-dom';
+
+import ChatIcon from "@mui/icons-material/Chat";
+import LockIcon from "@mui/icons-material/Lock";
+import VideocamIcon from "@mui/icons-material/Videocam";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import ScreenShareIcon from "@mui/icons-material/ScreenShare";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+
+
 export default function LandingPage() {
 
   
@@ -42,35 +51,34 @@ export default function LandingPage() {
 
       <div className="cardContainer">
         <div className="card">
-          <p>icon</p>
+          <ChatIcon fontSize="large" className="text-blue-500" />
           <h3>Real-Time Chat</h3>
           <p>Send instant messages,images,and files.</p>
         </div>
         <div className="card">
-          <p>icon</p>
+          <VideocamIcon fontSize="large" className="text-blue-500" />
           <h3>Live Video Calls</h3>
           <p>Connect face-to-face with HD video calls.</p>
         </div>
         <div className="card">
-          <p>icon</p>
+          <LockIcon fontSize="large" className="text-blue-500" />
           <h3>Secure Groups</h3>
           <p>Create private chat groups with passwords.</p>
         </div>
         <div className="card">
-          <p>icon</p>
-          <h3>Real-Time Chat</h3>
-          <p>Send instant messages,images,and files.</p>
+          <PersonAddIcon fontSize="large" className="text-blue-500" />
+          <h3>Join as Guest</h3>
+          <p>No signup required, join meetings instantly.</p>
         </div>
         <div className="card">
-          <p>icon</p>
-          <h3>Real-Time Chat</h3>
-          <p>Send instant messages,images,and files.</p>
+          <ScreenShareIcon fontSize="large" className="text-blue-500" />
+          <h3>Screen Sharing</h3>
+          <p>Share your screen for better collaboration.</p>
         </div>
         <div className="card">
-          <p>icon</p>
-          
-          <h3>Real-Time Chat</h3>
-          <p>Send instant messages,images,and files.</p>
+          <FiberManualRecordIcon fontSize="large" className="text-blue-500" />
+          <h3>Record Meetings</h3>
+          <p>Save and watch meetings later.</p>
         </div>
       </div>
 
